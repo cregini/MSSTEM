cf90403a64eb4b71e35dfe3b7616551e5485e5e6
reginius214 <32001992+cregini@users.noreply.github.com>
2025-01-08 11:05:35 -0500

    Update Voltage to pH Graph.py

--- Voltage to pH Graph.py
+++ Voltage to pH Graph.py
@@ -10,7 +10,7 @@ pH_values = [10.0, 7.0, 4.0]  #This is a list of corresponding pH values.  Note 
 graph = False  # create a variable to hold a state flag
 
 while True:
-voltage = round(eh.analog.one.read(), 2)
+	voltage = round(eh.analog.one.read(), 2)
 	print(f”The voltage is {voltage} V. Press pad 1 when stabilized.”)
 	sleep(1)
 	if eh.touch.one.is_pressed():
@@ -18,16 +18,16 @@ voltage = round(eh.analog.one.read(), 2)
 		print(f”The voltage {voltage} is being converted into a pH reading.”)
 	if graph == True:
 		graph = False # Lower the state flag
-#create a line graph from your experimental data
-plt.plot(voltages, pH_values, ‘bo-’) #blue dots connected with a line
-plt.xlabel(‘voltage (V)’)
-plt.ylabel(‘pH’)
-plt.title(‘Analog Voltage to pH Conversion’)
+	#create a line graph from your experimental data
+	plt.plot(voltages, pH_values, ‘bo-’) #blue dots connected with a line
+	plt.xlabel(‘voltage (V)’)
+	plt.ylabel(‘pH’)
+	plt.title(‘Analog Voltage to pH Conversion’)
 
-#Interpolate your data to estimate the pH of your sensor to the nearest tenth
-pH = round(np.interp(voltage, voltages, pH_values),1)
+	#Interpolate your data to estimate the pH of your sensor to the nearest tenth
+	pH = round(np.interp(voltage, voltages, pH_values),1)
 
-plt.plot(voltage, pH, ‘r^’) # plots the pH as a red triangle
-plt.annotate(str(pH), xy= (reading, pH + 0.25)) # labels the interpolated mark
+	plt.plot(voltage, pH, ‘r^’) # plots the pH as a red triangle
+	plt.annotate(str(pH), xy= (reading, pH + 0.25)) # labels the interpolated mark
 
-plt.show() # Displays the graph on the screen for visualization
+	plt.show() # Displays the graph on the screen for visualization
